ae9ca84efb68b333f159e2d38d6895c22cacdc74
KyubuC <105219784+KyubuC@users.noreply.github.com>
2022-12-26 13:00:48 +0800

    Add files via upload

---
+++ Midterm.js
@@ -0,0 +1,3 @@
+var bgColorArray = ['https://s3-us-west-2.amazonaws.com/s.cdpn.io/130527/bg1.png','https://s3-us-west-2.amazonaws.com/s.cdpn.io/130527/bg2.png'],
+    selectBG = bgColorArray[Math.floor(Math.random() * bgColorArray.length)];
+
