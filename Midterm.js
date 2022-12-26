d644555a5f561c33e36d8b858d1e19d74210d26b
KyubuC <105219784+KyubuC@users.noreply.github.com>
2022-12-26 23:11:47 +0800

    Delete Midterm.js

--- Midterm.js
+++
@@ -1,3 +0,0 @@
-var bgColorArray = ['https://s3-us-west-2.amazonaws.com/s.cdpn.io/130527/bg1.png','https://s3-us-west-2.amazonaws.com/s.cdpn.io/130527/bg2.png'],
-    selectBG = bgColorArray[Math.floor(Math.random() * bgColorArray.length)];
-
